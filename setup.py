c0923ad0de530d333a684b94b641e15db0a46f25
Yaşar Arabacı <yasar11732@gmail.com>
2012-01-08 01:51:37 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='packagegraph',
+      version='1.0',
+      description='Graph pacman activity',
+      author='Yaşar Arabacı',
+      author_email='yasar11732@gmail.com',
+      url='yasararabaci.tumbler.com',
+      scripts=['totalpackgraph', 'packgraph']
+     )
